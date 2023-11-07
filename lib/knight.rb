df7ce48ef1ba3e2a116935b1da2e9e2200d1ab2d
melinaDaendliker <melina.daendliker@hotmail.com>
2023-11-07 18:28:06 +0100

    add corrections with rubocop

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,79 +1,78 @@
+# frozen_string_literal: true
+
 class Knight
+  attr_accessor :start, :goal, :graph
 
-  attr_accessor :graph, :start, :goal
-
-  def initialize(graph={}, start, goal)
+  def initialize(start, goal, graph = {})
     @graph = graph
     @start = start
-    @goal = goal 
+    @goal = goal
   end
 
-  def allowed_moves(cur_position=@start)
-
+  def allowed_moves(cur_position = @start)
     game = Game.new
     board = game.create_board
     possible_new_position = []
     x_position = cur_position[0]
     y_position = cur_position[1]
 
-    possible_new_position.push([x_position-2, y_position-1])
-    possible_new_position.push([x_position-2, y_position+1])
-    possible_new_position.push([x_position-1, y_position-2])
-    possible_new_position.push([x_position-1, y_position+2])
+    possible_new_position.push([x_position - 2, y_position - 1])
+    possible_new_position.push([x_position - 2, y_position + 1])
+    possible_new_position.push([x_position - 1, y_position - 2])
+    possible_new_position.push([x_position - 1, y_position + 2])
 
-    possible_new_position.push([x_position+2, y_position-1])
-    possible_new_position.push([x_position+2, y_position+1])
-    possible_new_position.push([x_position+1, y_position-2])
-    possible_new_position.push([x_position+1, y_position+2])
+    possible_new_position.push([x_position + 2, y_position - 1])
+    possible_new_position.push([x_position + 2, y_position + 1])
+    possible_new_position.push([x_position + 1, y_position - 2])
+    possible_new_position.push([x_position + 1, y_position + 2])
 
     possible_new_position.select! { |e| board.include?(e) }
     possible_new_position
+  end
 
-  end 
-
-  def make_dict(start=@start, goal=@goal, knight_moves = {})
-  
+  def make_dict(start = @start, goal = @goal, knight_moves = {})
     return knight_moves if start == goal
 
     queue = [start]
     knight_moves[start] = allowed_moves(start)
 
-    while !queue.empty?
+    until queue.empty?
       current = queue.shift
       break if current == goal
 
-      for i in knight_moves[current] do
+      (knight_moves[current]).each do |i|
         next if knight_moves[i]
+
         knight_moves[i] = allowed_moves(i)
         queue.push(i)
       end
-    end 
+    end
     knight_moves
-  end 
+  end
 
-  def find_shortes_path(start=@start, goal=@goal, knight_moves)
+  def find_shortes_path(knight_moves, start = @start, goal = @goal)
     queue = [[start]]
-    while !queue.empty?
+    until queue.empty?
       path = queue.shift
       current_position = path.last
       return path if current_position == goal
-      for i in knight_moves[current_position] do
-        if !path.include?(i)
+
+      (knight_moves[current_position]).each do |i|
+        unless path.include?(i)
           new_path = path + [i]
           queue << new_path
         end
-      end 
-    end 
-  end 
+      end
+    end
+  end
 
-  def pretty_print(start=@start, goal=@goal)
+  def pretty_print(start = @start, goal = @goal)
     movement_dict = make_dict(start, goal)
-    shortest_path = find_shortes_path(start, goal, movement_dict)
-    path_length = shortest_path.length 
-    shortest_path[0]
-    puts "You made it in #{path_length -1} moves! Here is your path:"
-    for i in shortest_path
-      p i 
-    end 
-  end 
-end 
+    shortest_path = find_shortes_path(movement_dict, start, goal)
+    path_length = shortest_path.length
+    puts "You made it in #{path_length - 1} moves! Here is your path:"
+    shortest_path.each do |i|
+      p i
+    end
+  end
+end
